14cb72597e218c21ad0d59947d77d2860d39f481
jdbbaugh <jbaugh78@msn.com>
2019-01-04 15:19:11 -0600

    can do basic console log via first name search

--- src/scripts/carToDom.js
+++ src/scripts/carToDom.js
@@ -3,35 +3,45 @@ import salesByWeek from "./carData";
 const carToDom = {
   printToDom () {
     const targetContainer = document.querySelector(".output");
-    salesByWeek.forEach(sale => {
-      console.log(sale);
-      const saleContainer = document.createElement("article");
-      saleContainer.classList.add("sales-container");
-      targetContainer.appendChild(saleContainer);
-      const salesPerson = document.createElement("h2");
-      salesPerson.classList.add("salesPerson")
-      salesPerson.textContent = `${sale.sales_agent.first_name} ${sale.sales_agent.last_name}`
-      saleContainer.appendChild(salesPerson);
+    const inputBox = document.createElement("input");
+    inputBox.setAttribute("id","searchInput");
+    targetContainer.appendChild(inputBox);
+    const searchInput = document.querySelector("#searchInput");
+    searchInput.addEventListener("keypress", event => {
+      if (event.charCode === 13) {
+        const foundDealer = salesByWeek.find(element =>
+           element.sales_agent.first_name.includes(event.target.value)
+        );
 
-      for (const entry of Object.entries(sale.vehicle)) {
-        // console.log(entry);
-        const saleInfo = document.createElement("p");
-        saleInfo.textContent = `${entry[0]}: ${entry[1]}`
-        saleContainer.appendChild(saleInfo);
-      };
-      const profit = document.createElement("h2");
-      profit.classList.add("profit-container")
-      profit.textContent= `Profit: $${sale.gross_profit}`;
-      saleContainer.appendChild(profit);
-      saleContainer.appendChild(document.createElement("hr"));
-    })
+        console.log(foundDealer);
+        
+      }
+    });
+
+    
+    // salesByWeek.forEach(sale => {
+    //   // console.log(sale);
+    //   const saleContainer = document.createElement("article");
+    //   saleContainer.classList.add("sales-container");
+    //   targetContainer.appendChild(saleContainer);
+    //   const salesPerson = document.createElement("h2");
+    //   salesPerson.classList.add("salesPerson")
+    //   salesPerson.textContent = `${sale.sales_agent.first_name} ${sale.sales_agent.last_name}`
+    //   saleContainer.appendChild(salesPerson);
+
+    //   for (const entry of Object.entries(sale.vehicle)) {
+    //     // console.log(entry);
+    //     const saleInfo = document.createElement("p");
+    //     saleInfo.textContent = `${entry[0]}: ${entry[1]}`
+    //     saleContainer.appendChild(saleInfo);
+    //   };
+    //   const profit = document.createElement("h2");
+    //   profit.classList.add("profit-container")
+    //   profit.textContent= `Profit: $${sale.gross_profit}`;
+    //   saleContainer.appendChild(profit);
+    //   saleContainer.appendChild(document.createElement("hr"));
+    // })
   },
-  searchForSalesman () {
-    const targetContainer = document.querySelector(".output");
-    const inputElement = document.createElement("input");
-    inputElement.setAttribute("id", "searchInput");
-    targetContainer.appendChild(inputElement);
-  }
 };
 
 export default carToDom
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,4 @@
 import carToDom from "./carToDom"
 
-carToDom.searchForSalesman();
+carToDom.printToDom()
 
-const searchInput = document.querySelector("#searchInput");
-searchInput.addEventListener("keypress",  (event) => {
-  console.log(event.target.value)
-  if (event.charCode === 13) {
-    const searchTerm = event.target.value
-
-    salesByWeek.forEach(sale => {
-
-    });
-  }
-})
